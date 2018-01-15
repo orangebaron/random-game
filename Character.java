e1f1a2e6f9333313dcccf2e423bebcdf3fcd83e6
samalws <saalws@ctemc.org>
2018-01-15 12:41:59 -0500

    maxhealth is final

--- Character.java
+++ Character.java
@@ -1,5 +1,6 @@
 public abstract class Character {
-  private int health,maxHealth;
+  public final int maxHealth;
+  private int health;
   public final Sprite sprite;
   Position position;
   public Character(int health, Position position, Sprite sprite) {
@@ -13,5 +14,4 @@ public abstract class Character {
   void dealDamage(int d) { health -= d; if (health < 0) kill(); }
   void heal(int d) { health += d; if (health > maxHealth) health = maxHealth; }
   int getHealth() { return health; }
-  int getMaxHealth() { return maxHealth; }
 }
